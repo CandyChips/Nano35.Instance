16a3a94eaae04a40cd5b9b80d0e4879755ada52b
CandyChips <Crazy@killer-b.ru>
2021-04-13 20:43:00 +0300

    Added ef test

--- Nano35.Instance.Api/Helpers/PhoneConverter.cs
+++ Nano35.Instance.Api/Helpers/PhoneConverter.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 
 namespace Nano35.Instance.Api.Helpers
 {
@@ -6,12 +7,15 @@ namespace Nano35.Instance.Api.Helpers
     {
         public static string RuPhoneConverter(string currentPhone)// Comes like +7(800)555-35-35
         {
-            var tmp = currentPhone
+            var tmp = string.Concat(currentPhone
                 .AsEnumerable()
                 .Select(f => f.ToString())
-                .Where(f => int.TryParse(f, out _))
-                .ToString();
-            return tmp?.Substring(1); 
+                .Where(f => int.TryParse(f, out _)));
+            
+            if (tmp.Length < 11 || tmp.Length > 11)
+                return null;
+            
+            return tmp;
         }
         
     }
--- Nano35.Instance.Tests/UnitTest1.cs
+++ Nano35.Instance.Tests/UnitTest1.cs
@@ -1,4 +1,11 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Nano35.Instance.Api.Helpers;
+using Nano35.Instance.Processor.Models;
+using Nano35.Instance.Processor.Services.Contexts;
 using Xunit;
 
 namespace Nano35.Instance.Tests
@@ -7,7 +14,62 @@ namespace Nano35.Instance.Tests
     {
         [Fact]
         public void Test1()
+        {            
+            // Arrange
+            var phonesInit = new List<string>
+            {
+                "+79115339541", 
+                "89517380259", 
+                "89517380259123", 
+                "8951738025", 
+            };
+
+            var phonesNormal = new List<string>
+            {
+                "79115339541", 
+                "89517380259", 
+                null,
+                null
+            };
+
+            // Act
+            var result = phonesInit.Select(PhoneConverter.RuPhoneConverter).ToList();
+ 
+            // Assert
+            Assert.Equal(result, phonesNormal);
+        }
+    }
+    
+    /// <summary>
+    /// https://stackoverflow.com/questions/38890269/how-to-isolate-ef-inmemory-database-per-xunit-test
+    /// </summary>
+    public class UnitTest2
+    {
+        private ApplicationContext _context;
+        private void Initialize()
         {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationContext>();
+            optionsBuilder.UseInMemoryDatabase("test");
+            _context = new ApplicationContext(optionsBuilder.Options);
+        }
+        
+        [Fact]
+        public void Test2()
+        {            
+            // Arrange
+            Initialize();
+            var types = new List<ClientType>()
+            {
+                new ClientType() {Id = new Guid(), Name = "Type1"},
+                new ClientType() {Id = new Guid(), Name = "Type2"}
+            };
+
+            // Act
+            _context.ClientTypes.AddRange(types);
+            _context.SaveChanges();
+ 
+            // Assert
+            Assert.Equal(_context.ClientTypes.ToList(), types);
         }
     }
 }
